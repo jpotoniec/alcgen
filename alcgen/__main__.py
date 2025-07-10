c6d3477c4e9a9d9525c601ed012712e9196993b6
Jędrzej Potoniec <Jedrzej.Potoniec@cs.put.poznan.pl>
2025-07-10 14:25:45 +0200

    Code used for profiling

--- alcgen/__main__.py
+++ alcgen/__main__.py
@@ -2,15 +2,45 @@ import fire
 import numpy as np
 
 from alcgen.generator import generate
+from alcgen.guide import Guide
 from alcgen.random_guide import RandomGuide
+from alcgen.syntax import to_pretty
 
+
+class BaselineGuide(Guide):
+    def n_conjuncts(self, depth: int, universal: bool) -> int:
+        return 2
+
+    def n_disjuncts(self, depth: int, universal: bool) -> int:
+        if universal:
+            return 0
+        else:
+            return 2
+
+    def existential_roles(self, depth: int, n_roles: int, universal: bool) -> list[tuple[int, int]]:
+        return [(1, depth - 1), (1, depth - 1)]
+
+    def universal_roles(self, depth: int, roles: dict[int, list[int]], universal: bool) -> list[tuple[int, int]]:
+        return [(1, depth - 1)]
 
 def main():
-    for depth in range(1, 10):
-        for i in range(100):
+    class MyGuide(BaselineGuide):
+        def existential_roles(self, depth: int, n_roles: int, universal: bool) -> list[tuple[int, int]]:
+            return [(1, depth - 1)] * 5
+
+        def universal_roles(self, depth: int, roles: dict[int, list[int]], universal: bool) -> list[tuple[int, int]]:
+            return []
+
+    ce = generate(5, MyGuide(), True, True)
+    assert ce is not None
+    return
+
+    for depth in range(5, 6):
+        for i in range(1):
             print(depth, i)
             guide = RandomGuide(np.random.default_rng(0xfeed * i + 0xbad * depth))
-            generate(depth, guide, True, True)
+            ce = generate(depth, guide, True, True)
+            print(to_pretty(ce))
     # guide = RandomGuide(np.random.default_rng(0xfeed))
     # n = Generator().generate(2, guide)
     # print(n.debug())
