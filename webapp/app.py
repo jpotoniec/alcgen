41c7464a1bd2c5e91981d9993ef7cac98c104eff
Jędrzej Potoniec <Jedrzej.Potoniec@cs.put.poznan.pl>
2025-07-14 11:51:32 +0200

    webapp: minor improvmenets

--- webapp/app.py
+++ webapp/app.py
@@ -12,7 +12,8 @@ from alcgen.syntax import to_manchester, to_pretty
 
 def config(key_prefix: str, disabled: bool, closed: bool) -> RandomGuideConfiguration | None:
     min_n_conjuncts = 2 if closed else 1
-    n_conjuncts = st.slider("Conjuncts count", min_value=min_n_conjuncts, max_value=10, value=(min_n_conjuncts, 3), step=1, disabled=disabled,
+    n_conjuncts = st.slider("Conjuncts count", min_value=min_n_conjuncts, max_value=10, value=(min_n_conjuncts, 3),
+                            step=1, disabled=disabled,
                             key=f"{key_prefix}_n_conjuncts")
     p_disjuncts = st.slider("Disjuncts probability", min_value=0.0, max_value=1.0, step=0.1, disabled=disabled,
                             key=f"{key_prefix}_p_disjuncts")
@@ -44,20 +45,26 @@ def config(key_prefix: str, disabled: bool, closed: bool) -> RandomGuideConfigur
                                     )
 
 
-depth = st.slider("Depth", min_value=0, max_value=10, step=1, value=2)
-minimize = st.checkbox("Minimize", value=True)
-close = st.checkbox("Close", value=True)
-seed = st.number_input("Seed", min_value=0, max_value=(1 << 32) - 1, value=secrets.randbits(32))
-prefix = st.text_input("Prefix", value="http://example.com/foo#")
+st.title(r"$\mathcal{ALC}$ random formula generator")
+
+depth = st.slider("ABox depth", min_value=0, max_value=10, step=1, value=2)
+minimize = st.checkbox("Minimize the number of different atomic classes in the formula", value=True,
+                       help="If left unchecked, a single atomic class will not appear twice in the resulting formula. If checked, some symbols may be repeated. The minimization process is heuristic and there is not guarantee that the final number of distinct atomic classes is minimal in any sense.")
+close = st.checkbox("Make the formula unsatisfiable", value=True,
+                    help="If checked, the resulting formula will be unsatisfiable. Otherwise, the formula will be satisfiable.")
+seed = st.number_input("Random seed", min_value=0, max_value=(1 << 32) - 1, value=secrets.randbits(32), help="The seed to be used in the random number generator. The same seed with the same configuration will yield the same formula.")
+prefix = st.text_input("Prefix (for OWL serialization)", value="http://example.com/alcgen#",
+                       help=f"Prefix to be used in the OWL serialization. The generated class will be identified by the local name D.")
 
 col1, col2 = st.columns(2)
 
 with col1:
+    st.checkbox("Basic configuration", value=True, disabled=True)
     base_config = config("base", False, close)
     use_universals = base_config.universal_threshold_low is not None and base_config.universal_threshold_high is not None
 
 with col2:
-    use_universal_config = st.checkbox("Use different parameters in universals", value=False,
+    use_universal_config = st.checkbox("Use different parameters in universal restrictions", value=False,
                                        disabled=not use_universals)
     universal_config = config("universal", not use_universal_config, close)
 
@@ -68,6 +75,7 @@ st.download_button("Download config",
                                              save_closed=not minimize and close,
                                              save_closed_minimized=minimize and close,
                                              seed_depth=0, seed_instance=0, seed_const=seed,
+                                             prefix=prefix,
                                              guide=base_config,
                                              universal_guide=universal_config).model_dump_json(),
                    mime="application/json", file_name="config.json")
