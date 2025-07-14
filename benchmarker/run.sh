16f2e25181af358526fb420c85e6db5a237b0404
Jędrzej Potoniec <Jedrzej.Potoniec@cs.put.poznan.pl>
2025-07-14 09:51:56 +0200

    run.sh: use appropriate extensions (jsonl instead of json)

--- benchmarker/run.sh
+++ benchmarker/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-java --add-opens=java.base/java.lang=ALL-UNNAMED -jar JFactBenchmarker/target/JFactBenchmarker-1.0-SNAPSHOT.jar "$@" |tee jfact.json
-java -jar PelletBenchmarker/target/PelletBenchmarker-1.0-SNAPSHOT.jar "$@" |tee pellet.json
-java -jar HermiTBenchmarker/target/HermiTBenchmarker-1.0-SNAPSHOT.jar "$@" |tee hermit.json
-java -jar ELKBenchmarker/target/ELKBenchmarker-1.0-SNAPSHOT.jar "$@" |tee elk.json
+java --add-opens=java.base/java.lang=ALL-UNNAMED -jar JFactBenchmarker/target/JFactBenchmarker-1.0-SNAPSHOT.jar "$@" |tee jfact.jsonl
+java -jar PelletBenchmarker/target/PelletBenchmarker-1.0-SNAPSHOT.jar "$@" |tee pellet.jsonl
+java -jar HermiTBenchmarker/target/HermiTBenchmarker-1.0-SNAPSHOT.jar "$@" |tee hermit.jsonl
+java -jar ELKBenchmarker/target/ELKBenchmarker-1.0-SNAPSHOT.jar "$@" |tee elk.jsonl
