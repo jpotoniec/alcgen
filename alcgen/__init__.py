6cbf2e3a9b68fed7032a12ac6f20bbadba2a7b74
Jędrzej Potoniec <Jedrzej.Potoniec@cs.put.poznan.pl>
2025-07-23 16:02:37 +0200

    Refactoring

---
+++ alcgen/__init__.py
@@ -0,0 +1,5 @@
+from .generator import Generator, generate, do_minimize, do_close
+from .guide import Guide
+from .node import Node
+from .random_guide import RandomGuide
+from .syntax import to_manchester, to_pretty, SUB, EQV, DIS, NOT, AND, OR, ALL, ANY, FORALL, EXISTS
--- alcgen/__main__.py
+++ alcgen/__main__.py
@@ -6,36 +6,6 @@ import fire
 
 from alcgen.configuration import DatasetConfiguration
 from alcgen.create_dataset import create_dataset
-from alcgen.generator import generate
-from alcgen.guide import Guide
-
-
-def simple_benchmark():
-    class BaselineGuide(Guide):
-        def n_conjuncts(self, depth: int, universal: bool) -> int:
-            return 2
-
-        def n_disjuncts(self, depth: int, universal: bool) -> int:
-            if universal:
-                return 0
-            else:
-                return 2
-
-        def existential_roles(self, depth: int, n_roles: int, universal: bool) -> list[tuple[int, int]]:
-            return [(1, depth - 1), (1, depth - 1)]
-
-        def universal_roles(self, depth: int, roles: dict[int, list[int]], universal: bool) -> list[tuple[int, int]]:
-            return [(1, depth - 1)]
-
-    class MyGuide(BaselineGuide):
-        def existential_roles(self, depth: int, n_roles: int, universal: bool) -> list[tuple[int, int]]:
-            return [(1, depth - 1)] * 5
-
-        def universal_roles(self, depth: int, roles: dict[int, list[int]], universal: bool) -> list[tuple[int, int]]:
-            return []
-
-    ce = generate(5, MyGuide(), True, True)
-    assert ce is not None
 
 
 def main(config_file: os.PathLike, target_dir: os.PathLike | None = None):
