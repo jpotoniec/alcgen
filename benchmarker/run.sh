1bc33713aba3a1d39bd876f0f9f7abe8ddd7bda7
Jędrzej Potoniec <Jedrzej.Potoniec@cs.put.poznan.pl>
2025-07-23 15:27:26 +0200

    Benchmarker: more robust runner

--- benchmarker/run.sh
+++ benchmarker/run.sh
@@ -1,6 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-java --add-opens=java.base/java.lang=ALL-UNNAMED -jar JFactBenchmarker/target/JFactBenchmarker-1.0-SNAPSHOT.jar "$@" |tee jfact.jsonl
-java -jar PelletBenchmarker/target/PelletBenchmarker-1.0-SNAPSHOT.jar "$@" |tee pellet.jsonl
-java -jar HermiTBenchmarker/target/HermiTBenchmarker-1.0-SNAPSHOT.jar "$@" |tee hermit.jsonl
-java -jar ELKBenchmarker/target/ELKBenchmarker-1.0-SNAPSHOT.jar "$@" |tee elk.jsonl
+prefix=$1
+shift
+
+ulimit -t 600
+ulimit -m $((32*1024*1024))
+JAVAOPS=-Xmx16G
+
+java $JAVAOPS --add-opens=java.base/java.lang=ALL-UNNAMED -jar JFactBenchmarker/target/JFactBenchmarker-1.0-SNAPSHOT.jar "$@" |tee "$prefix"jfact.jsonl
+java $JAVAOPS -jar PelletBenchmarker/target/PelletBenchmarker-1.0-SNAPSHOT.jar "$@" |tee "$prefix"pellet.jsonl
+java $JAVAOPS -jar HermiTBenchmarker/target/HermiTBenchmarker-1.0-SNAPSHOT.jar "$@" |tee "$prefix"hermit.jsonl
+java $JAVAOPS -jar ELKBenchmarker/target/ELKBenchmarker-1.0-SNAPSHOT.jar "$@" |tee "$prefix"elk.jsonl
