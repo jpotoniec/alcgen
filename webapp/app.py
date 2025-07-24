97c5fff940984be1bb9c0401151557a0cfb1ac7d
Jędrzej Potoniec <Jedrzej.Potoniec@cs.put.poznan.pl>
2025-07-24 13:57:19 +0200

    webapp: minor fixes

--- webapp/app.py
+++ webapp/app.py
@@ -1,3 +1,8 @@
+import sys
+from pathlib import Path
+
+sys.path.append(Path(__file__).parent.parent.absolute().as_posix())
+
 import io
 import secrets
 
@@ -12,17 +17,17 @@ from alcgen.syntax import to_manchester, to_pretty
 
 def config(key_prefix: str, disabled: bool, closed: bool) -> RandomGuideConfiguration | None:
     min_n_conjuncts = 2 if closed else 1
-    n_conjuncts = st.slider("Conjuncts count", min_value=min_n_conjuncts, max_value=10, value=(min_n_conjuncts, 3),
+    n_conjuncts = st.slider("Conjuncts count", min_value=min_n_conjuncts, max_value=5, value=(min_n_conjuncts, 3),
                             step=1, disabled=disabled,
                             key=f"{key_prefix}_n_conjuncts")
     p_disjuncts = st.slider("Disjuncts probability", min_value=0.0, max_value=1.0, step=0.1, disabled=disabled,
                             key=f"{key_prefix}_p_disjuncts")
-    n_disjuncts = st.slider("Disjuncts count", min_value=2, max_value=10, value=(2, 2), step=1,
+    n_disjuncts = st.slider("Disjuncts count", min_value=2, max_value=5, value=(2, 2), step=1,
                             disabled=disabled or p_disjuncts <= 0.0, key=f"{key_prefix}_n_disjuncts")
     can_use_existential = depth > 0
-    n_existential = st.slider("Existential restrictions count", min_value=0, max_value=10, value=(0, 3), step=1,
+    n_existential = st.slider("Existential restrictions count", min_value=0, max_value=5, value=(0, 3), step=1,
                               disabled=disabled or not can_use_existential, key=f"{key_prefix}_n_existential")
-    n_roles = st.slider("Maximal number of roles", min_value=1, max_value=10, value=1, step=1,
+    n_roles = st.slider("Maximal number of roles", min_value=1, max_value=5, value=1, step=1,
                         disabled=disabled or not can_use_existential, key=f"{key_prefix}_n_roles")
     can_use_universal = can_use_existential and n_existential[1] > 0
     use_universals = st.checkbox("Universal restrictions", value=can_use_universal,
@@ -47,12 +52,13 @@ def config(key_prefix: str, disabled: bool, closed: bool) -> RandomGuideConfigur
 
 st.title(r"$\mathcal{ALC}$ random formula generator")
 
-depth = st.slider("ABox depth", min_value=0, max_value=10, step=1, value=2)
+depth = st.slider("ABox depth", min_value=0, max_value=5, step=1, value=2)
 minimize = st.checkbox("Minimize the number of different atomic classes in the formula", value=True,
                        help="If left unchecked, a single atomic class will not appear twice in the resulting formula. If checked, some symbols may be repeated. The minimization process is heuristic and there is not guarantee that the final number of distinct atomic classes is minimal in any sense.")
 close = st.checkbox("Make the formula unsatisfiable", value=True,
                     help="If checked, the resulting formula will be unsatisfiable. Otherwise, the formula will be satisfiable.")
-seed = st.number_input("Random seed", min_value=0, max_value=(1 << 32) - 1, value=secrets.randbits(32), help="The seed to be used in the random number generator. The same seed with the same configuration will yield the same formula.")
+seed = st.number_input("Random seed", min_value=0, max_value=(1 << 32) - 1, value=secrets.randbits(32),
+                       help="The seed to be used in the random number generator. The same seed with the same configuration will yield the same formula.")
 prefix = st.text_input("Prefix (for OWL serialization)", value="http://example.com/alcgen#",
                        help=f"Prefix to be used in the OWL serialization. The generated class will be identified by the local name D.")
 
