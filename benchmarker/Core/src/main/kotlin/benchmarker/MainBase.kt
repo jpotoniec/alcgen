abd578135e4400ce5f3e8838afa8e77c2f7b7e3b
Jędrzej Potoniec <Jedrzej.Potoniec@cs.put.poznan.pl>
2025-09-02 17:17:24 +0200

    Revert "Benchmarking on no_disjuncts without JIT"
    This reverts commit 6e29b322cd520130dafc559d028082b24d37b797.

--- benchmarker/Core/src/main/kotlin/benchmarker/MainBase.kt
+++ benchmarker/Core/src/main/kotlin/benchmarker/MainBase.kt
@@ -21,21 +21,20 @@ abstract class MainBase {
     fun run(args: Array<String>): Unit {
         val clsIri = IRI.create(args[0])
         val reps = args[1].toInt()
-        val times = HashMap<String, ArrayList<Duration?>>()
-        repeat(reps) {
-            for (ontologyFile in args.sliceArray(2..<args.size)) {
-                val m = OWLManager.createOWLOntologyManager()
-                val ontology = m.loadOntology(IRI.create(File(ontologyFile)))
-                val cls = m.owlDataFactory.getOWLClass(clsIri)
+        for (ontologyFile in args.sliceArray(2..<args.size)) {
+            val m = OWLManager.createOWLOntologyManager()
+            val ontology = m.loadOntology(IRI.create(File(ontologyFile)))
+            val cls = m.owlDataFactory.getOWLClass(clsIri)
+            val times = ArrayList<Duration?>()
+            repeat(reps) {
                 System.gc()
                 val time = measureTime {
                     val reasoner: OWLReasoner = factory(ontology)
                     reasoner.isSatisfiable(cls)
                 }
-                times.computeIfAbsent(ontologyFile) { ArrayList() }.add(time)
+                times.add(time)
             }
+            println(Json.encodeToString(Result(ontologyFile, times.map { it?.inWholeNanoseconds ?: -1 })))
         }
-        for ((ontologyFile, t) in times.entries)
-            println(Json.encodeToString(Result(ontologyFile, t.map { it?.inWholeNanoseconds ?: -1 })))
     }
 }
