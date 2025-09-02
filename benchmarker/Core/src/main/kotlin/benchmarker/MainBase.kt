6e29b322cd520130dafc559d028082b24d37b797
Jędrzej Potoniec <Jedrzej.Potoniec@cs.put.poznan.pl>
2025-09-02 17:16:54 +0200

    Benchmarking on no_disjuncts without JIT

--- benchmarker/Core/src/main/kotlin/benchmarker/MainBase.kt
+++ benchmarker/Core/src/main/kotlin/benchmarker/MainBase.kt
@@ -21,20 +21,21 @@ abstract class MainBase {
     fun run(args: Array<String>): Unit {
         val clsIri = IRI.create(args[0])
         val reps = args[1].toInt()
-        for (ontologyFile in args.sliceArray(2..<args.size)) {
-            val m = OWLManager.createOWLOntologyManager()
-            val ontology = m.loadOntology(IRI.create(File(ontologyFile)))
-            val cls = m.owlDataFactory.getOWLClass(clsIri)
-            val times = ArrayList<Duration?>()
-            repeat(reps) {
+        val times = HashMap<String, ArrayList<Duration?>>()
+        repeat(reps) {
+            for (ontologyFile in args.sliceArray(2..<args.size)) {
+                val m = OWLManager.createOWLOntologyManager()
+                val ontology = m.loadOntology(IRI.create(File(ontologyFile)))
+                val cls = m.owlDataFactory.getOWLClass(clsIri)
                 System.gc()
                 val time = measureTime {
                     val reasoner: OWLReasoner = factory(ontology)
                     reasoner.isSatisfiable(cls)
                 }
-                times.add(time)
+                times.computeIfAbsent(ontologyFile) { ArrayList() }.add(time)
             }
-            println(Json.encodeToString(Result(ontologyFile, times.map { it?.inWholeNanoseconds ?: -1 })))
         }
+        for ((ontologyFile, t) in times.entries)
+            println(Json.encodeToString(Result(ontologyFile, t.map { it?.inWholeNanoseconds ?: -1 })))
     }
 }
